288c321900719aeb4a8d22b60ce6bfbde66d3828
Divashen <divashen@ovex.io>
2025-11-10 21:41:15 +0200

    Depot Scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Done only if storing files on disk - sets the current host and protocol that Active Storage uses for generating URLs
+  include ActiveStorage::SetCurrent
 end
